08deb5ef472e840c2d5498b18357b88030d0e9eb
muchu1983 <muchu1983@gmail.com>
2024-03-31 13:34:42 +0800

    test use pylint

--- key_play_mp3.py
+++ key_play_mp3.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import keyboard
-from playsound import playsound
+"""
+module docstring
+"""
+
 import threading
 import os
+import keyboard
+from playsound import playsound
+
 
 def play_sound(mp3_path):
     print(mp3_path)
@@ -14,10 +19,10 @@ def key_press(key_event):
     if key_event.event_type == 'down':
         match key_event.name:
             case 'f12':
-                mp3_path = os.path.abspath('E:\\my_desktop\\bark.mp3')
+                mp3_path = os.path.abspath('E:\\my_desktop\\py_work\\key-play-mp3\\mp3\\bark.mp3')
                 threading.Thread(target=play_sound, args=(mp3_path,)).start()
             case 'f11':
-                mp3_path = os.path.abspath('E:\\my_desktop\\howl.mp3')
+                mp3_path = os.path.abspath('E:\\my_desktop\\py_work\\key-play-mp3\\mp3\\howl.mp3')
                 threading.Thread(target=play_sound, args=(mp3_path,)).start()
 
 if __name__ == '__main__':
@@ -25,3 +30,4 @@ if __name__ == '__main__':
     keyboard.hook(key_press)
     keyboard.wait()
     print('[stop] hook keyboard')
+
